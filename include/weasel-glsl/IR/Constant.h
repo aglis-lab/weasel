1621032911bd84d0070661bbbe0324254c5e448a
zaen <gooner0709@gmail.com>
2022-08-28 19:22:16 +0700

    Add AST GLSL Header

---
+++ include/weasel-glsl/IR/Constant.h
@@ -0,0 +1,17 @@
+/**
+ * @file Constant.h
+ * @author zaen (gooner0709@gmail.com)
+ * @brief
+ * @version 0.1
+ * @date 2022-08-28
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+namespace weasel
+{
+    namespace GLSL
+    {
+    } // namespace GLSL
+} // namespace weasel
